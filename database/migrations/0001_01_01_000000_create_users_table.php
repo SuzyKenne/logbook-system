aff38843f80f9fb5404347dc282fc44cee1a4e8f
SuzyKenne <suzyndatewo@gmail.com>
2025-06-05 14:04:38 +0100

    updated user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
